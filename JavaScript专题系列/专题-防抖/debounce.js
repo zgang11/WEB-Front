fdd68b6190dd46d9f3452374e74566170ef2e611
zgang11 <704527318@qq.com>
2019-08-29 17:31:30 +0800

    编辑

--- JavaScript专题系列/专题-防抖/debounce.js
+++ JavaScript专题系列/专题-防抖/debounce.js
@@ -7,7 +7,7 @@ function getUserAction(e) {
     container.innerHTML = count++;
 };
 
-
+//防抖：尽管触发了事件，但一定要在事件出发n秒后执行。如果在这n秒内再次出发事件，就以新事件的时间开始，n秒后再次执行这个函数
 //第一版
 function debounce(func,wait){
     
@@ -19,7 +19,7 @@ function debounce(func,wait){
 }
 
 
-//第二版
+//第二版：改变this的指向问题
 function debounce2(func,wait){
     var timeout
     
@@ -33,7 +33,7 @@ function debounce2(func,wait){
 }
 
 
-//第三版
+//第三版：修改event对象，此时防抖已经趋于完美了。
 function debounce3(func,wait){
     var timeout
     return function(){
@@ -48,6 +48,7 @@ function debounce3(func,wait){
  
 
 //第四版
+//需求：出发后立即执行函数，等到出发n秒后，才可以重新触发。
 function debounce4(func,wait,immediate){
     var timeout
     return function(){
