94b4882a73fe4b28fa8b9bf9cbf1f50171ffb906
zhanggang56 <45088871+zgang11@users.noreply.github.com>
2021-04-13 14:42:08 +0800

    react提交

---
+++ my-react/src/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import LikeButton from './myTest/state.js'
+import LikeButtonProp from './myTest/props'
+import Student from './higher_Com/higherCom'
+import Hello from './life_circle/life_circle'
+import Circle from './react-life-function/1'
+
+
+/**
+ * react-link:
+ * --https://juejin.cn/post/6844903842430074893
+ */
+
+
+class Header extends Component {
+  render(){
+    const word = 'is good'
+    return (
+      <div>
+        <Title/>
+        <h1 className={word}>xjjalaf {word}</h1>
+      </div>
+    )
+  }
+}
+
+class Title extends Component {
+  handleClick(a,b){
+    console.log('click on title')
+    console.log(this,a,b)
+  }
+  render(){
+    return (
+        <div>
+          <div onClick={this.handleClick.bind(this,'hello','fat ass')}>title1</div>,
+          <div>title2</div>,
+          <div>title3</div>
+          <LikeButton/>
+          <LikeButtonProp likeText='喜欢' unLikeText='不喜欢' onClick={()=>{console.log('hit me')}}  wording={{likeText:'like',unLikeText:'no like'}}/>
+          <LikeButtonProp/>
+        </div>
+    )
+  }
+}
+
+class HelloWorld extends Component{
+  render(){
+    return (
+      <>'j-ajajaj'</>
+    )
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <Circle></Circle>,
+  </div>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
