168c4f3fb1330814c5f9f06cabcce5b0b4156bcc
zhanggang56 <zhanggang56@jd.com>
2021-06-01 08:59:30 +0800

    删除不要文件

--- webpack/demo0009/main.js
+++
@@ -1,5 +0,0 @@
-document.write('hello  yahoo')
-
-if(__DEV__){
-	document.write(new Date())
-}
--- webpack/demo0012/main2.jsx
+++
@@ -1,6 +0,0 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
-ReactDOM .render(
-   <h1>hello b</h1>,
-   document.getElementById('b')
-) 
